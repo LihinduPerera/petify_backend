3753b9b33a13fc19b50db7f5c279ddc3ee9faa07
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-03-17 17:20:43 +0530

    updated auth

--- schema/user.py
+++ schema/user.py
@@ -1,17 +1,18 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class UserIn(BaseModel):
     name: str
     email: str
     password: str
-    address: str = None
-    phone: str = None
+    address: Optional[str] = None
+    phone: Optional[str] = None
 
 class UserOut(BaseModel):
     name: str
     email: str
-    address: str = None
-    phone: str = None
+    address: Optional[str] = None
+    phone: Optional[str] = None
 
 class UserLogin(BaseModel):
     email: str
