20734aa001f3324a5fd8d623c71eee4f5492836b
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-03-18 13:37:46 +0530

    allow_origins

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from routes.auth import router as auth_router
 from routes.products import router as product_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
@@ -10,3 +12,11 @@ app.include_router(product_router , tags=["products"])
 @app.get("/")
 def read_root():
     return {"message": "Welcome to PETIFY fastAPI"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
